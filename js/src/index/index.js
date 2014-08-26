c091ae68ad72c53753b0303c7dcdba0c3d9f0894
rainyjune <rainyjune@live.cn>
2014-08-26 10:43:37 +0800

    Add files

---
+++ js/src/index/index.js
@@ -0,0 +1,18 @@
+﻿define(function (require, exports, module) {
+  function initialize() {
+    var position = new AMap.LngLat(116.397428, 39.90923);
+    var mapObj = new AMap.Map("container", {
+      view: new AMap.View2D({
+        center: position,
+        zoom: 14,
+        rotation: 0
+      }),
+      lang: "zh_cn"
+    });
+  }
+
+  $(document).ready(function () {
+    initialize();
+  });
+  
+});
